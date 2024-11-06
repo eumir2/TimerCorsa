8780386fdb531f87d2c828990af94070c8e3e2c0
Eumir Cometti <eumircometti.ec.ec@gmail.com>
2024-11-06 19:14:08 +0100

    Add interface

--- app/src/main/java/com/example/timercorsa/MainActivity.kt
+++ app/src/main/java/com/example/timercorsa/MainActivity.kt
@@ -17,31 +17,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TimerCorsaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main);
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TimerCorsaTheme {
-        Greeting("Android")
     }
 }
